c3359853bb8921b668e323b454896399d31b5c0b
taylorkraft <taylorkraft16@gmail.com>
2020-05-04 11:40:37 -0600

    establish model relationships

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,6 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
-  belongs_to :user
+  belongs_to :user #creator, singular user instances
+  has_many :ratings
+  has_many :users, through: :ratings #people who have rated beers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :ratings
+  has_many :rated_beers, through: :ratings, source: :beer #we can't have a has_many and has_many, through: with the same name. source allows us to rename 'beers' to 'rated_beers' 
+
+  has_many :beers #that they have created
 end
