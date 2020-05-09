965c502705e9f14fc10c95e60e92c44983e81d1f
taylorkraft <taylorkraft16@gmail.com>
2020-05-08 20:29:14 -0600

    beer controller actions/show page

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,7 +3,18 @@ class Beer < ApplicationRecord
   belongs_to :user #creator, singular user instances
   has_many :ratings
   has_many :users, through: :ratings #people who have rated beers
-  accepts_nested_attributes_for :brewery
+  # accepts_nested_attributes_for :brewery
 
   validates :name, presence: true
+  #validate :not_a_duplicate
+
+  validates :name, uniqueness: {scope: :brewery, message: "has already been added"}
+
+  def self.alpha
+    order(:name)
+  end
+
+  def brewery_attributes=(attributes)
+    brewery = Brewery.find_or_create_by(attributes) if !attributes['name'].empty?
+  end
 end
