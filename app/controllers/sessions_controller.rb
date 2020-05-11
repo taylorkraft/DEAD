b50501ff07f9fbba7e459e186f43825b8b884c97
taylorkraft <taylorkraft16@gmail.com>
2020-05-10 18:59:14 -0600

    update create session action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,19 +12,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:user][:username]) #try to find user in the system
-    # if @user && @user.authenticate(params[:user][:password]) 
-    if @user.try(:authenticate, params[:user][:password]) #did we find a user and did they enter a valid password?
+    if params [:provider] == 'google_oauth2'
+      @user = User.create_by_google_omniauth(auth)
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:error] = "Sorry, login failed. Please try again."
-      redirect_to login_path
+      @user = User.find_by(username: params[:user][:username]) #try to find user in the system
+      # if @user && @user.authenticate(params[:user][:password]) 
+      if @user.try(:authenticate, params[:user][:password]) #did we find a user and did they enter a valid password?
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:error] = "Sorry, login failed. Please try again."
+        redirect_to login_path
+      end
     end
   end
 
   def omniauth
-    byebug
+
     
   end
 end
