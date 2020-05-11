cefed6e0284c2c88f2c442857d848dc3e66e9011
taylorkraft <taylorkraft16@gmail.com>
2020-05-10 19:03:09 -0600

    add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_172115) do
+ActiveRecord::Schema.define(version: 2020_05_11_010138) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_172115) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "beers", "breweries"
