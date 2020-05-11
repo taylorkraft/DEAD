c077b86396c82eaa0ee231632b4317e44e9a1b62
taylorkraft <taylorkraft16@gmail.com>
2020-05-11 10:42:06 -0600

    add links to edit

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
     resources :ratings, only: [:new, :index]
   end
   resources :breweries
-  resources :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
